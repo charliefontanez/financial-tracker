8ceb03fafbed3f163e75938ccc169763379a1b4f
Owen Taylor <Owentaylor60@yahoo.com>
2022-05-26 03:57:31 -0400

    inital sequelize

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS financial_tracker_db;
+
+CREATE DATABASE financial_tracker_db;
+
+USE financial_tracker_db;
--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-console.log("Node is working");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
