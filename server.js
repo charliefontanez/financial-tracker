0d9281dc1ea1aa823a9b5a34e764c35dbfef6cb2
Christian Foote <christianfoote999@gmail.com>
2022-05-31 18:50:24 -0400

    added get route for finance

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const routes = require('./controllers');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const path = require('path');
+
 const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,3 +25,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
